780fbc3fcd822f150e0524176b52690b886a3c1c
Stefan de Konink <stefan@konink.de>
2014-08-18 17:21:15 +0200

    Speed up the postcode search by a text_ops index, change user to postgres for copy, add order by for postcodes.

--- bin/export-bag.sh
+++ bin/export-bag.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "copy (select distinct substring(postcode from 1 for 4) from adres) to '/tmp/bag_csv/postcodes4pp.csv';" | psql -U bag bag
+echo "CREATE INDEX adres_postcode ON adres (postcode varchar_pattern_ops);" | psql -U postgres bag
+
+echo "copy (select distinct substring(postcode from 1 for 4) as pp4 from adres order by pp4) to '/tmp/bag_csv/postcodes4pp.csv';" | psql -U postgres bag
 
 cat /tmp/bag_csv/postcodes4pp.csv | while read i; do
-	echo "copy (select openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging, postcode, woonplaatsnaam, gemeentenaam, provincienaam, typeadresseerbaarobject, cast(ST_X(ST_Transform(geopunt, 4326)) as numeric(9,6)) as longitude, cast(ST_Y(ST_Transform(geopunt, 4326)) as numeric(8,6)) as latitude from adres where postcode like '$i%' order by postcode, openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging) to '/tmp/bag_csv/$i.csv' csv header;" | psql -U bag bag
+	echo "copy (select openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging, postcode, woonplaatsnaam, gemeentenaam, provincienaam, typeadresseerbaarobject, cast(ST_X(ST_Transform(geopunt, 4326)) as numeric(9,6)) as longitude, cast(ST_Y(ST_Transform(geopunt, 4326)) as numeric(8,6)) as latitude from adres where postcode like '$i%' order by postcode, openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging) to '/tmp/bag_csv/$i.csv' csv header;" | psql -U postgres bag
 done
 
