4c46916674fe02a34895f3f9385d9acefbba7f26
Stefan de Konink <stefan@konink.de>
2018-11-30 21:37:32 +0100

    Update to 12-11-2018 BAG database

--- bin/export-bag.sh
+++ bin/export-bag.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "CREATE INDEX adres_postcode ON bagactueel.adres (postcode varchar_pattern_ops);" | sudo -u postgres psql
+echo "CREATE INDEX adres_postcode ON bagactueel.adres (postcode varchar_pattern_ops);" | psql -U postgres
 
-echo "copy (select distinct substring(postcode from 1 for 4) as pp4 from bagactueel.adres order by pp4) to '/tmp/bag_csv/postcodes4pp.csv';" | sudo -u postgres psql
+echo "\\copy (select distinct substring(postcode from 1 for 4) as pp4 from bagactueel.adres order by pp4) to '/tmp/bag_csv/postcodes4pp.csv';" | psql -U postgres
 
 cat /tmp/bag_csv/postcodes4pp.csv | while read i; do
-	echo "copy (select distinct openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging, postcode, woonplaatsnaam, gemeentenaam, provincienaam, typeadresseerbaarobject, cast(ST_X(ST_Transform(geopunt, 4326)) as numeric(9,6)) as longitude, cast(ST_Y(ST_Transform(geopunt, 4326)) as numeric(8,6)) as latitude from bagactueel.adres where postcode like '$i%' order by postcode, openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging) to '/tmp/bag_csv/$i.csv' csv header;" | sudo -u postgres psql
+	echo "\\copy (select distinct openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging, postcode, woonplaatsnaam, gemeentenaam, provincienaam, typeadresseerbaarobject, cast(ST_X(ST_Transform(geopunt, 4326)) as numeric(9,6)) as longitude, cast(ST_Y(ST_Transform(geopunt, 4326)) as numeric(8,6)) as latitude from bagactueel.adres where postcode like '$i%' order by postcode, openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging) to '/tmp/bag_csv/$i.csv' csv header;" | psql -U postgres psql
 done
 
